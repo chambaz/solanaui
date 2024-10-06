14edd5b5f50a474f388a52093247b0091d579f22
chambaz <adammchambers@gmail.com>
2024-10-06 00:41:28 -0400

    feat: working web3auth example

--- src/components/sol/connect-wallet-dialog.tsx
+++ src/components/sol/connect-wallet-dialog.tsx
@@ -2,7 +2,22 @@
 
 import React from "react";
 
+import Image from "next/image";
+
 import { Wallet } from "@solana/wallet-adapter-react";
+import {
+  CHAIN_NAMESPACES,
+  WALLET_ADAPTERS,
+  WEB3AUTH_NETWORK,
+} from "@web3auth/base";
+import { Web3AuthNoModal } from "@web3auth/no-modal";
+import { SolanaPrivateKeyProvider } from "@web3auth/solana-provider";
+import { AuthAdapter } from "@web3auth/auth-adapter";
+import {
+  IconBrandX,
+  IconBrandGoogle,
+  IconBrandDiscord,
+} from "@tabler/icons-react";
 
 import { useWallet } from "@/hooks/use-wallet";
 
@@ -15,6 +30,30 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const web3AuthChainConfig = {
+  chainNamespace: CHAIN_NAMESPACES.SOLANA,
+  chainId: "0x1",
+  rpcTarget: process.env.NEXT_PUBLIC_RPC_URL as string,
+  displayName: "Solana Mainnet",
+  blockExplorerUrl: "https://solscan.io",
+  ticker: "SOL",
+  tickerName: "Solana",
+};
+
+const web3AuthPrivateKeyProvider = new SolanaPrivateKeyProvider({
+  config: { chainConfig: web3AuthChainConfig },
+});
+
+const web3auth = new Web3AuthNoModal({
+  clientId: process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID as string,
+  web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
+  privateKeyProvider: web3AuthPrivateKeyProvider,
+});
+
+const web3AuthAdapter = new AuthAdapter();
+web3auth.configureAdapter(web3AuthAdapter);
 
 const ConnectWalletDialogTrigger = DialogTrigger;
 
@@ -28,29 +67,85 @@ const ConnectWalletDialogContent = React.forwardRef<
   const { wallets, select, connect, connected, connecting, setIsOpen } =
     useWallet();
 
+  const socialWallets = [
+    {
+      name: "X",
+      icon: <IconBrandX />,
+    },
+    {
+      name: "Google",
+      icon: <IconBrandGoogle />,
+    },
+    {
+      name: "Discord",
+      icon: <IconBrandDiscord />,
+    },
+  ];
+
+  const login = async () => {
+    const web3authProvider = await web3auth.connectTo(WALLET_ADAPTERS.AUTH, {
+      loginProvider: "google",
+    });
+    if (web3auth.connected) {
+      console.log("connected");
+    }
+  };
+
   React.useEffect(() => {
     if (!selectedWallet || connected || connecting) return;
     connect();
     setIsOpen(false);
   }, [selectedWallet, connect, connected, connecting, setIsOpen]);
 
+  React.useEffect(() => {
+    const init = async () => {
+      await web3auth.init();
+    };
+    init();
+  }, []);
+
   return (
     <DialogContent ref={ref} {...props}>
       {children}
-      <div>
-        {wallets.map((wallet: Wallet) => (
-          <Button
-            key={wallet.adapter.name}
-            onClick={() => {
-              select(wallet.adapter.name);
-              setSelectedWallet(wallet);
-            }}
-            disabled={connecting}
-          >
-            {wallet.adapter.name}
-          </Button>
+      <form className="flex gap-2">
+        <Input placeholder="Email address" />
+        <Button variant="outline" type="submit">
+          Sign In
+        </Button>
+      </form>
+      <ul className="flex justify-center gap-2">
+        {socialWallets.map((wallet) => (
+          <li key={wallet.name}>
+            <Button
+              variant="outline"
+              className="relative"
+              title={`Connect with ${wallet.name}`}
+              onClick={login}
+            >
+              {wallet.icon}
+            </Button>
+          </li>
         ))}
-      </div>
+        {wallets.map((wallet: Wallet) => (
+          <li key={wallet.adapter.name}>
+            <Button
+              variant="outline"
+              onClick={() => {
+                select(wallet.adapter.name);
+                setSelectedWallet(wallet);
+              }}
+              disabled={connecting}
+            >
+              <Image
+                src={wallet.adapter.icon}
+                alt={wallet.adapter.name}
+                width={20}
+                height={20}
+              />
+            </Button>
+          </li>
+        ))}
+      </ul>
     </DialogContent>
   );
 });

d1b2ca92b5927c7a656b59cf51a4d2476fe36342
chambaz <adammchambers@gmail.com>
2025-01-18 20:26:23 -0500

    chore: remove wallet connect alert from docs tabs

--- src/components/web/docs-tabs.tsx
+++ src/components/web/docs-tabs.tsx
@@ -2,15 +2,10 @@
 
 import React from "react";
 
-import { useWallet } from "@solana/wallet-adapter-react";
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import tsx from "react-syntax-highlighter/dist/esm/languages/prism/tsx";
 import ColdDark from "react-syntax-highlighter/dist/esm/styles/prism/coldark-dark";
-import { IconRocket } from "@tabler/icons-react";
 
-import { ConnectWallet } from "@/components/web/connect-wallet";
-
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import {
   Select,
   SelectContent,
@@ -31,25 +26,15 @@ type DocsVariant = {
 
 type DocsTabsProps = {
   variants: DocsVariant[];
-  showConnectWalletAlert?: boolean;
 };
 
 SyntaxHighlighter.registerLanguage("tsx", tsx);
 
-const DocsTabs = ({
-  variants,
-  showConnectWalletAlert = true,
-}: DocsTabsProps) => {
-  const { connected, connecting } = useWallet();
+const DocsTabs = ({ variants }: DocsTabsProps) => {
   const [activeVariantIndex, setActiveVariantIndex] = React.useState<number>(0);
   const [activeVariant, setActiveVariant] = React.useState<DocsVariant>(
     variants[activeVariantIndex],
   );
-  const [isMounted, setIsMounted] = React.useState(false);
-
-  React.useEffect(() => {
-    setIsMounted(true);
-  }, []);
 
   React.useEffect(() => {
     setActiveVariant(variants[activeVariantIndex]);
@@ -57,20 +42,6 @@ const DocsTabs = ({
 
   return (
     <div className="mt-6 space-y-6">
-      {!connected && !connecting && isMounted && showConnectWalletAlert && (
-        <Alert>
-          <IconRocket size={20} />
-          <AlertTitle>Heads up!</AlertTitle>
-          <AlertDescription>
-            <ConnectWallet>
-              <button className="border-b border-primary text-primary">
-                Connect your wallet
-              </button>
-            </ConnectWallet>{" "}
-            to enable all example features and demos.
-          </AlertDescription>
-        </Alert>
-      )}
       <Tabs defaultValue="preview">
         <TabsList>
           <TabsTrigger value="preview">Preview</TabsTrigger>

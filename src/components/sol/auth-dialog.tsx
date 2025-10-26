fb82f163040150b75540b79d860fbe2854c13876
Adam Chambers <adammchambers@gmail.com>
2025-10-26 09:18:01 -0400

    chore: install shadcn, configure basic docs page

---
+++ src/components/sol/auth-dialog.tsx
@@ -0,0 +1,35 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+const AuthDialog = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>AuthDialog!</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>AuthDialog</DialogTitle>
+          <DialogDescription>AuthDialog description</DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DialogClose>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export { AuthDialog };
